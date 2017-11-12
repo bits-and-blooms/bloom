7d4edd8590387a98502815093bcfb9162b244155
Rob Skillington <r@uber.com>
2017-11-12 17:35:34 -0500

    Fork to add read only bloom filter and concurrent read only bloom filter

--- bloom_test.go
+++ bloom_test.go
@@ -3,71 +3,81 @@ package bloom
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/gob"
-	"encoding/json"
+	"fmt"
 	"math"
+	"runtime"
+	"sync"
 	"testing"
+
+	"github.com/spaolacci/murmur3"
+
+	"github.com/stretchr/testify/require"
 )
 
-// This implementation of Bloom filters is _not_
-// safe for concurrent use. Uncomment the following
-// method and run go test -race
-//
-// func TestConcurrent(t *testing.T) {
-// 	gmp := runtime.GOMAXPROCS(2)
-// 	defer runtime.GOMAXPROCS(gmp)
-//
-// 	f := New(1000, 4)
-// 	n1 := []byte("Bess")
-// 	n2 := []byte("Jane")
-// 	f.Add(n1)
-// 	f.Add(n2)
-//
-// 	var wg sync.WaitGroup
-// 	const try = 1000
-// 	var err1, err2 error
-//
-// 	wg.Add(1)
-// 	go func() {
-// 		for i := 0; i < try; i++ {
-// 			n1b := f.Test(n1)
-// 			if !n1b {
-// 				err1 = fmt.Errorf("%v should be in", n1)
-// 				break
-// 			}
-// 		}
-// 		wg.Done()
-// 	}()
-//
-// 	wg.Add(1)
-// 	go func() {
-// 		for i := 0; i < try; i++ {
-// 			n2b := f.Test(n2)
-// 			if !n2b {
-// 				err2 = fmt.Errorf("%v should be in", n2)
-// 				break
-// 			}
-// 		}
-// 		wg.Done()
-// 	}()
-//
-// 	wg.Wait()
-//
-// 	if err1 != nil {
-// 		t.Fatal(err1)
-// 	}
-// 	if err2 != nil {
-// 		t.Fatal(err2)
-// 	}
-// }
+// TestConcurrent must be run with -race to detect failures
+func TestConcurrent(t *testing.T) {
+	gmp := runtime.GOMAXPROCS(2)
+	defer runtime.GOMAXPROCS(gmp)
+
+	f := NewBloomFilter(1000, 4)
+	n1 := []byte("Bess")
+	n2 := []byte("Jane")
+	f.Add(n1)
+	f.Add(n2)
+
+	buf := bytes.NewBuffer(nil)
+	require.NoError(t, f.BitSet().Write(buf))
+
+	ro := NewConcurrentReadOnlyBloomFilter(f.M(), f.K(), buf.Bytes())
+	require.Equal(t, f.M(), ro.M())
+	require.Equal(t, f.K(), ro.K())
+
+	var wg sync.WaitGroup
+	const try = 1000
+	var err1, err2 error
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < try; i++ {
+			n1b := ro.Test(n1)
+			if !n1b {
+				err1 = fmt.Errorf("%v should be in", n1)
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < try; i++ {
+			n2b := ro.Test(n2)
+			if !n2b {
+				err2 = fmt.Errorf("%v should be in", n2)
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	if err1 != nil {
+		t.Fatal(err1)
+	}
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+}
 
 func TestBasic(t *testing.T) {
-	f := New(1000, 4)
+	f := NewBloomFilter(1000, 4)
 	n1 := []byte("Bess")
 	n2 := []byte("Jane")
 	n3 := []byte("Emma")
 	f.Add(n1)
-	n3a := f.TestAndAdd(n3)
+	n3a := f.Test(n3)
+	f.Add(n3)
 	n1b := f.Test(n1)
 	n2b := f.Test(n2)
 	n3b := f.Test(n3)
@@ -85,8 +95,37 @@ func TestBasic(t *testing.T) {
 	}
 }
 
+func TestReadOnly(t *testing.T) {
+	f := NewBloomFilter(1000, 4)
+	n1 := []byte("Bess")
+	n2 := []byte("Jane")
+	n3 := []byte("Emma")
+	f.Add(n1)
+	f.Add(n3)
+
+	buf := bytes.NewBuffer(nil)
+	require.NoError(t, f.BitSet().Write(buf))
+
+	ro := NewReadOnlyBloomFilter(f.M(), f.K(), buf.Bytes())
+	require.Equal(t, f.M(), ro.M())
+	require.Equal(t, f.K(), ro.K())
+
+	n1b := ro.Test(n1)
+	n2b := ro.Test(n2)
+	n3b := ro.Test(n3)
+	if !n1b {
+		t.Errorf("%v should be in.", n1)
+	}
+	if n2b {
+		t.Errorf("%v should not be in.", n2)
+	}
+	if !n3b {
+		t.Errorf("%v should be in the second time we look.", n3)
+	}
+}
+
 func TestBasicUint32(t *testing.T) {
-	f := New(1000, 4)
+	f := NewBloomFilter(1000, 4)
 	n1 := make([]byte, 4)
 	n2 := make([]byte, 4)
 	n3 := make([]byte, 4)
@@ -96,7 +135,8 @@ func TestBasicUint32(t *testing.T) {
 	binary.BigEndian.PutUint32(n3, 102)
 	binary.BigEndian.PutUint32(n4, 103)
 	f.Add(n1)
-	n3a := f.TestAndAdd(n3)
+	n3a := f.Test(n3)
+	f.Add(n3)
 	n1b := f.Test(n1)
 	n2b := f.Test(n2)
 	n3b := f.Test(n3)
@@ -116,7 +156,7 @@ func TestBasicUint32(t *testing.T) {
 }
 
 func TestNewWithLowNumbers(t *testing.T) {
-	f := New(0, 0)
+	f := NewBloomFilter(0, 0)
 	if f.k != 1 {
 		t.Errorf("%v should be 1", f.k)
 	}
@@ -126,17 +166,19 @@ func TestNewWithLowNumbers(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
-	f := NewWithEstimates(1000, 0.001)
+	m, k := EstimateFalsePositiveRate(1000, 0.001)
+	f := NewBloomFilter(m, k)
 	n1 := "Love"
 	n2 := "is"
 	n3 := "in"
 	n4 := "bloom"
-	f.AddString(n1)
-	n3a := f.TestAndAddString(n3)
-	n1b := f.TestString(n1)
-	n2b := f.TestString(n2)
-	n3b := f.TestString(n3)
-	f.TestString(n4)
+	f.Add([]byte(n1))
+	n3a := f.Test([]byte(n3))
+	f.Add([]byte(n3))
+	n1b := f.Test([]byte(n1))
+	n2b := f.Test([]byte(n2))
+	n3b := f.Test([]byte(n3))
+	f.Test([]byte(n4))
 	if !n1b {
 		t.Errorf("%v should be in.", n1)
 	}
@@ -149,29 +191,7 @@ func TestString(t *testing.T) {
 	if !n3b {
 		t.Errorf("%v should be in the second time we look.", n3)
 	}
-
 }
-
-func testEstimated(n uint, maxFp float64, t *testing.T) {
-	m, k := EstimateParameters(n, maxFp)
-	f := NewWithEstimates(n, maxFp)
-	fpRate := f.EstimateFalsePositiveRate(n)
-	if fpRate > 1.5*maxFp {
-		t.Errorf("False positive rate too high: n: %v; m: %v; k: %v; maxFp: %f; fpRate: %f, fpRate/maxFp: %f", n, m, k, maxFp, fpRate, fpRate/maxFp)
-	}
-}
-
-func TestEstimated1000_0001(t *testing.T)   { testEstimated(1000, 0.000100, t) }
-func TestEstimated10000_0001(t *testing.T)  { testEstimated(10000, 0.000100, t) }
-func TestEstimated100000_0001(t *testing.T) { testEstimated(100000, 0.000100, t) }
-
-func TestEstimated1000_001(t *testing.T)   { testEstimated(1000, 0.001000, t) }
-func TestEstimated10000_001(t *testing.T)  { testEstimated(10000, 0.001000, t) }
-func TestEstimated100000_001(t *testing.T) { testEstimated(100000, 0.001000, t) }
-
-func TestEstimated1000_01(t *testing.T)   { testEstimated(1000, 0.010000, t) }
-func TestEstimated10000_01(t *testing.T)  { testEstimated(10000, 0.010000, t) }
-func TestEstimated100000_01(t *testing.T) { testEstimated(100000, 0.010000, t) }
 
 func min(a, b uint) uint {
 	if a < b {
@@ -194,14 +214,16 @@ func min(a, b uint) uint {
 // test, comparing the result histogram with the uniform distribition.
 // This yields a test statistic with degrees-of-freedom of m-1.
 func chiTestBloom(m, k, rounds uint, elements [][]byte) (succeeds bool) {
-	f := New(m, k)
+	f := NewBloomFilter(m, k)
 	results := make([]uint, m)
 	chi := make([]float64, m)
 
+	hash := murmur3.New128()
 	for _, data := range elements {
-		h := baseHashes(data)
-		for i := uint(0); i < f.k; i++ {
-			results[f.location(h, i)]++
+		hash.Reset()
+		h := bloomFilterHashes(hash, data)
+		for i := uint64(0); i < f.k; i++ {
+			results[bloomFilterLocation(h, i, f.m)]++
 		}
 	}
 
@@ -251,347 +273,4 @@ func TestLocation(t *testing.T) {
 		t.Error("random assignment is too unrandom")
 	}
 
-}
-
-func TestCap(t *testing.T) {
-	f := New(1000, 4)
-	if f.Cap() != f.m {
-		t.Error("not accessing Cap() correctly")
-	}
-}
-
-func TestK(t *testing.T) {
-	f := New(1000, 4)
-	if f.K() != f.k {
-		t.Error("not accessing K() correctly")
-	}
-}
-
-func TestMarshalUnmarshalJSON(t *testing.T) {
-	f := New(1000, 4)
-	data, err := json.Marshal(f)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	var g BloomFilter
-	err = json.Unmarshal(data, &g)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if g.m != f.m {
-		t.Error("invalid m value")
-	}
-	if g.k != f.k {
-		t.Error("invalid k value")
-	}
-	if g.b == nil {
-		t.Fatal("bitset is nil")
-	}
-	if !g.b.Equal(f.b) {
-		t.Error("bitsets are not equal")
-	}
-}
-
-func TestUnmarshalInvalidJSON(t *testing.T) {
-	data := []byte("{invalid}")
-
-	var g BloomFilter
-	err := g.UnmarshalJSON(data)
-	if err == nil {
-		t.Error("expected error while unmarshalling invalid data")
-	}
-}
-
-func TestWriteToReadFrom(t *testing.T) {
-	var b bytes.Buffer
-	f := New(1000, 4)
-	_, err := f.WriteTo(&b)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	g := New(1000, 1)
-	_, err = g.ReadFrom(&b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if g.m != f.m {
-		t.Error("invalid m value")
-	}
-	if g.k != f.k {
-		t.Error("invalid k value")
-	}
-	if g.b == nil {
-		t.Fatal("bitset is nil")
-	}
-	if !g.b.Equal(f.b) {
-		t.Error("bitsets are not equal")
-	}
-
-	g.Test([]byte(""))
-}
-
-func TestReadWriteBinary(t *testing.T) {
-	f := New(1000, 4)
-	var buf bytes.Buffer
-	bytesWritten, err := f.WriteTo(&buf)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if bytesWritten != int64(buf.Len()) {
-		t.Errorf("incorrect write length %d != %d", bytesWritten, buf.Len())
-	}
-
-	var g BloomFilter
-	bytesRead, err := g.ReadFrom(&buf)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if bytesRead != bytesWritten {
-		t.Errorf("read unexpected number of bytes %d != %d", bytesRead, bytesWritten)
-	}
-	if g.m != f.m {
-		t.Error("invalid m value")
-	}
-	if g.k != f.k {
-		t.Error("invalid k value")
-	}
-	if g.b == nil {
-		t.Fatal("bitset is nil")
-	}
-	if !g.b.Equal(f.b) {
-		t.Error("bitsets are not equal")
-	}
-}
-
-func TestEncodeDecodeGob(t *testing.T) {
-	f := New(1000, 4)
-	f.Add([]byte("one"))
-	f.Add([]byte("two"))
-	f.Add([]byte("three"))
-	var buf bytes.Buffer
-	err := gob.NewEncoder(&buf).Encode(f)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	var g BloomFilter
-	err = gob.NewDecoder(&buf).Decode(&g)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if g.m != f.m {
-		t.Error("invalid m value")
-	}
-	if g.k != f.k {
-		t.Error("invalid k value")
-	}
-	if g.b == nil {
-		t.Fatal("bitset is nil")
-	}
-	if !g.b.Equal(f.b) {
-		t.Error("bitsets are not equal")
-	}
-	if !g.Test([]byte("three")) {
-		t.Errorf("missing value 'three'")
-	}
-	if !g.Test([]byte("two")) {
-		t.Errorf("missing value 'two'")
-	}
-	if !g.Test([]byte("one")) {
-		t.Errorf("missing value 'one'")
-	}
-}
-
-func TestEqual(t *testing.T) {
-	f := New(1000, 4)
-	f1 := New(1000, 4)
-	g := New(1000, 20)
-	h := New(10, 20)
-	n1 := []byte("Bess")
-	f1.Add(n1)
-	if !f.Equal(f) {
-		t.Errorf("%v should be equal to itself", f)
-	}
-	if f.Equal(f1) {
-		t.Errorf("%v should not be equal to %v", f, f1)
-	}
-	if f.Equal(g) {
-		t.Errorf("%v should not be equal to %v", f, g)
-	}
-	if f.Equal(h) {
-		t.Errorf("%v should not be equal to %v", f, h)
-	}
-}
-
-func BenchmarkEstimated(b *testing.B) {
-	for n := uint(100000); n <= 100000; n *= 10 {
-		for fp := 0.1; fp >= 0.0001; fp /= 10.0 {
-			f := NewWithEstimates(n, fp)
-			f.EstimateFalsePositiveRate(n)
-		}
-	}
-}
-
-func BenchmarkSeparateTestAndAdd(b *testing.B) {
-	f := NewWithEstimates(uint(b.N), 0.0001)
-	key := make([]byte, 100)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		binary.BigEndian.PutUint32(key, uint32(i))
-		f.Test(key)
-		f.Add(key)
-	}
-}
-
-func BenchmarkCombinedTestAndAdd(b *testing.B) {
-	f := NewWithEstimates(uint(b.N), 0.0001)
-	key := make([]byte, 100)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		binary.BigEndian.PutUint32(key, uint32(i))
-		f.TestAndAdd(key)
-	}
-}
-
-func TestMerge(t *testing.T) {
-	f := New(1000, 4)
-	n1 := []byte("f")
-	f.Add(n1)
-
-	g := New(1000, 4)
-	n2 := []byte("g")
-	g.Add(n2)
-
-	h := New(999, 4)
-	n3 := []byte("h")
-	h.Add(n3)
-
-	j := New(1000, 5)
-	n4 := []byte("j")
-	j.Add(n4)
-
-	err := f.Merge(g)
-	if err != nil {
-		t.Errorf("There should be no error when merging two similar filters")
-	}
-
-	err = f.Merge(h)
-	if err == nil {
-		t.Errorf("There should be an error when merging filters with mismatched m")
-	}
-
-	err = f.Merge(j)
-	if err == nil {
-		t.Errorf("There should be an error when merging filters with mismatched k")
-	}
-
-	n2b := f.Test(n2)
-	if !n2b {
-		t.Errorf("The value doesn't exist after a valid merge")
-	}
-
-	n3b := f.Test(n3)
-	if n3b {
-		t.Errorf("The value exists after an invalid merge")
-	}
-
-	n4b := f.Test(n4)
-	if n4b {
-		t.Errorf("The value exists after an invalid merge")
-	}
-}
-
-func TestCopy(t *testing.T) {
-	f := New(1000, 4)
-	n1 := []byte("f")
-	f.Add(n1)
-
-	// copy here instead of New
-	g := f.Copy()
-	n2 := []byte("g")
-	g.Add(n2)
-
-	n1fb := f.Test(n1)
-	if !n1fb {
-		t.Errorf("The value doesn't exist in original after making a copy")
-	}
-
-	n1gb := g.Test(n1)
-	if !n1gb {
-		t.Errorf("The value doesn't exist in the copy")
-	}
-
-	n2fb := f.Test(n2)
-	if n2fb {
-		t.Errorf("The value exists in the original, it should only exist in copy")
-	}
-
-	n2gb := g.Test(n2)
-	if !n2gb {
-		t.Errorf("The value doesn't exist in copy after Add()")
-	}
-}
-
-func TestFrom(t *testing.T) {
-	var (
-		k    = uint(5)
-		data = make([]uint64, 10)
-		test = []byte("test")
-	)
-
-	bf := From(data, k)
-	if bf.K() != k {
-		t.Errorf("Constant k does not match the expected value")
-	}
-
-	if bf.Cap() != uint(len(data)*64) {
-		t.Errorf("Capacity does not match the expected value")
-	}
-
-	if bf.Test(test) {
-		t.Errorf("Bloom filter should not contain the value")
-	}
-
-	bf.Add(test)
-	if !bf.Test(test) {
-		t.Errorf("Bloom filter should contain the value")
-	}
-
-	// create a new Bloom filter from an existing (populated) data slice.
-	bf = From(data, k)
-	if !bf.Test(test) {
-		t.Errorf("Bloom filter should contain the value")
-	}
-}
-
-func TestTestLocations(t *testing.T) {
-	f := NewWithEstimates(1000, 0.001)
-	n1 := []byte("Love")
-	n2 := []byte("is")
-	n3 := []byte("in")
-	n4 := []byte("bloom")
-	f.Add(n1)
-	n3a := f.TestLocations(Locations(n3, f.K()))
-	f.Add(n3)
-	n1b := f.TestLocations(Locations(n1, f.K()))
-	n2b := f.TestLocations(Locations(n2, f.K()))
-	n3b := f.TestLocations(Locations(n3, f.K()))
-	n4b := f.TestLocations(Locations(n4, f.K()))
-	if !n1b {
-		t.Errorf("%v should be in.", n1)
-	}
-	if n2b {
-		t.Errorf("%v should not be in.", n2)
-	}
-	if n3a {
-		t.Errorf("%v should not be in the first time we look.", n3)
-	}
-	if !n3b {
-		t.Errorf("%v should be in the second time we look.", n3)
-	}
-	if n4b {
-		t.Errorf("%v should be in.", n4)
-	}
 }
